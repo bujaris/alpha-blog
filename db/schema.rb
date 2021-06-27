8fd7c286203e4c459d20da0b4a16630cc790a163
Bujaris <ii.bujari@gmail.com>
2021-06-28 00:22:48 +0200

    Added Show article route

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_25_234606) do
+ActiveRecord::Schema.define(version: 2021_06_26_000130) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
